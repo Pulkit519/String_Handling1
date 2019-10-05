79c5a3104b8a9d924ded8274fdbe0e05519eb50b
Pulkit519 <pulkit.gla_cs18@gla.ac.in>
2019-10-05 18:54:25 +0530

    check the vowel and replaced them with '*' and Finally Program build up successfully

--- src/question_3/Question_3.java
+++ src/question_3/Question_3.java
@@ -6,5 +6,13 @@ public class Question_3 {
     public static void main(String[] args) {
         Scanner X = new Scanner(System.in);
         String s = X.nextLine().trim();
+        for(int i =0 ;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='a' || ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+               s=s.replace(ch,'*');
+            }
+        }
+        System.out.println(s);
+
     }
 }
