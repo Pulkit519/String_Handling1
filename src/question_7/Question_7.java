9628f96d2d78b451c0bd66b368f4784949dad633
Pulkit519 <pulkit.gla_cs18@gla.ac.in>
2019-10-05 19:24:23 +0530

    program build up successfully

---
+++ src/question_7/Question_7.java
@@ -0,0 +1,15 @@
+package question_7;
+
+import java.util.Scanner;
+
+public class Question_7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine().trim();
+        int l = s.length();
+        long a = 0;
+        for (int i = 0; i < l; i++)
+            a += 0 + s.charAt((i));
+        System.out.println("total weight=" + a);
+    }
+}
